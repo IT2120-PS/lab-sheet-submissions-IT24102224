f6e55aa5562110f60967d94b160812dc6bd4d6be
IT24102224 <it24102224@my.sliit.lk>
2025-09-19 16:34:16 +0530

    ADD LAB6

---
+++ Lab Sheet 06/IT24102224_PS_Lab06.R
@@ -0,0 +1,18 @@
+#Question 01
+#Part 1
+#Binomial Distribution
+#Random variable X has binomial distribution with n==50 and p=0.85
+
+#Part 2
+1-pbinom(46,50,0.85,lower.tail = TRUE)
+
+#Question 02
+#Part 1
+#Number of calls receives by the call center on a given hour
+
+#Part 2
+#Poisson distribution
+#Random variable X has poisson distribution with lambda = 12
+
+#Part 3
+dpois(15,12)
